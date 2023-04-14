a3258b38f37e7795ea22db3f3e375e284b6d73e6
UnaiDeV <unai.dcc@gmail.com>
2023-04-14 18:49:28 +0200

    FEATUREV2

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
             // RAMA MASTER V1
 
             // RAMA DEVELOP V1      // RAMA DEVELOP V2
-            // RAMA FEATURE1 V1 
+            // RAMA FEATURE1 V1        // RAMA FEATURE1 V2    
         
         Scanner lectura = new Scanner(System.in);
                 System.out.println("Introduce un número de euros para convertir");
